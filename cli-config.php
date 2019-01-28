f41dcb0ce60f364a2e9eb9eabeef1c09a7c10c21
Dmitry Golovachev <gdim447@gmail.com>
2019-01-28 18:34:55 +0700

    Add cli-config for Doctrine

---
+++ cli-config.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+/** @var \App\Kernel\Application $app */
+$app = require './bootstrap/app.php';
+
+$container = $app->container();
+
+$entityManager = $container->get(EntityManagerInterface::class);
+
+return ConsoleRunner::createHelperSet($entityManager);
