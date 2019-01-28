2abd4d2a010b61c99c10c1b0d7e1db2e4296ecca
Dmitry Golovachev <gdim447@gmail.com>
2019-01-28 18:40:49 +0700

    Add database config

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+return [
+    'driver'   => 'pdo_mysql',
+    'host' => 'mysql',
+    'user'     => 'richard',
+    'password' => 'secret',
+    'dbname'   => 'todos',
+];
